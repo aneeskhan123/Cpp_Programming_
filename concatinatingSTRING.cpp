78f67902ecc285f8def4ae2b6e9b0ed30f9d73df
aneeskhan123 <143631989+aneeskhan123@users.noreply.github.com>
2023-08-30 08:29:49 -0700

    Concatinate Strings

---
+++ concatinatingSTRING.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	string firstname="john",lastname="Doe";
+	
+	//Change/replacing character
+	//firstname[3]='K'; 
+	
+	cout<<firstname+lastname;
+	
+}
